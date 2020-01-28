b5bcc2e8c975824ea6ac927409b63d268ab0d335
Cancain <eriksson.tomas@gmail.com>
2020-01-28 19:20:45 +0100

    refactored settings class

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from pynput.keyboard import Key, Controller
 keyboard = Controller()
 
 class Settings:
-    input_key = ''
-    wait_time = 0
-    should_run = False
+    def __init__(self, input_key, wait_time):
+        self.input_key = input_key
+        self.wait_time = wait_time
+        self.should_run = False
 
 
 def press_key(settings: Settings):
@@ -18,9 +19,10 @@ def press_key(settings: Settings):
 
 
 def get_settings():
-    settings = Settings()
-    settings.input_key = input("What key?")
-    settings.wait_time = input("Wait time?")
+    settings = Settings(
+            input("What key?"),
+            input("what time?")
+            )
     return settings
 
 
