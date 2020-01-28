fb9aaf82d4801517fa13e760a104955e71e5a6c4
Cancain <eriksson.tomas@gmail.com>
2020-01-28 19:47:03 +0100

    started on adding support for walking

--- main.py
+++ main.py
@@ -11,18 +11,40 @@ class Settings:
         self.input_key = input_key
         self.wait_time = wait_time
         self.should_run = False
-
+        self.walk_forwards = False
+        self.repeats = 0
 
 def press_key(settings: Settings):
-    keyboard.press(str(settings.input_key))
-    time.sleep(int(settings.wait_time))
+    for i in range(int(settings.repeats)):
+        keyboard.press(str(settings.input_key))
+        time.sleep(int(settings.wait_time))
 
+def check_walk():
+    walk_forwards = False
+    valid_response = False
+
+    while not valid_response:
+        valid_response = True
+        walk_input = input("walk forwards? (Y/N)")
+        if (walk_input == "Y" or walk_input == "y"):
+            walk_forwards = True
+        elif (walk_input == "N" or walk_input == "n"):
+            walk_forwards = False
+        else:
+            print("Ansver Y or N")
+            valid_response = False
+
+    return walk_forwards
 
 def get_settings():
     settings = Settings(
             input("What key?"),
-            input("what time?")
+            input("what time?"),
             )
+    
+    settings.walk_forwards = check_walk()
+    if settings.walk_forwards:
+        settings.repeats = input("how many repeats before walking?")
     return settings
 
 
